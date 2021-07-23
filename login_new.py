825983a56d875bfbea0298fc2ccbf5e10c87afe8
situ2001 <yongcong2001@outlook.com>
2021-07-23 20:22:18 +0800

    debug

--- login_new.py
+++ login_new.py
@@ -870,6 +870,7 @@ def login_new(usr: str, pwd: str):
     res = session.get(urls.cas_new, verify=False)
     lt = re.findall(r'name="lt" value="(.*)"', res.text)
     enc_target = usr + pwd + lt[0]
+    print(enc_target)
     result_rsa = ctx.call('strEnc', enc_target, '1', '2', '3')
 
     form = {
@@ -880,8 +881,6 @@ def login_new(usr: str, pwd: str):
         'execution': 'e1s1',
         '_eventId': 'submit',
     }
-
-    print(form)
 
     res = session.post(urls.cas_new, data=form)
 
