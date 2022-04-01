df70632378b2874410474ff11a2241a8d21cb687
Yuzi <2205816944@qq.com>
2022-04-01 13:12:47 +0800

    modified counter

--- run.py
+++ run.py
@@ -23,7 +23,8 @@ def main():
     print('开始打卡...')
     print()
 
-    MAX_RETRY_COUNT = 10
+    MAX_RETRY_COUNT_LOGIN = 4
+    MAX_RETRY_COUNT_CLOCKIN = 10
 
     summary = {}
 
@@ -33,24 +34,24 @@ def main():
         clock_in_status = False
         try:
             print("登录中...")
-            count = 1
+            count_login = 1
             login_status = login_new(id, students[id])
-            while not login_status and count <= MAX_RETRY_COUNT:
-                print("登录失败 重试中 次数:", count)
+            while not login_status and count_login <= MAX_RETRY_COUNT_LOGIN:
+                print("登录失败 重试中 次数:", count_login)
                 login_status = login_new(id, students[id])
-                count += 1
+                count_login += 1
             print("打卡中...")
-            count = 1
+            count_clockin = 1
             clock_in_status = clock_in(id)
-            while not clock_in_status and count <= MAX_RETRY_COUNT:
-                print("打卡失败 重试中 次数:", count)
+            while not clock_in_status and count_clockin <= MAX_RETRY_COUNT_CLOCKIN:
+                print("打卡失败 重试中 次数:", count_clockin)
                 login_status = login_new(id, students[id])
-                while not login_status and count <= MAX_RETRY_COUNT:
-                    print("登录失败 重试中 次数:", count)
+                while not login_status and count_login <= MAX_RETRY_COUNT_LOGIN:
+                    print("登录失败 重试中 次数:", count_login)
                     login_status = login_new(id, students[id])
-                    count += 1
+                    count_login += 1
                 clock_in_status = clock_in(id)
-                count += 1
+                count_clockin += 1
         except:
             print("失败了，当前学号", id, "\n")
         summary[id] = (login_status, clock_in_status)
@@ -76,37 +77,40 @@ def main():
             'https://sctapi.ftqq.com/{}.send'.format(key), params=payload)
 
     # if PPTKEY is set
-    if os.getenv('PPTKEY'): 
-        token=os.getenv('PPTKEY')
+    if os.getenv('PPTKEY'):
+        token = os.getenv('PPTKEY')
         payload = {
-            'token': token ,
-            'title': '健康' + result_str ,
-            'content': result_str ,
+            'token': token,
+            'title': '健康' + result_str,
+            'content': result_str,
             'template': 'html'
         }
-        requests.get('http://www.pushplus.plus/send' , params=payload)
-    
+        requests.get('http://www.pushplus.plus/send', params=payload)
+
     # if you have termux-api
-    if os.name == 'posix' :
-        os.system('[ -x $PREFIX/libexec/termux-api ]&&termux-notification -t "{}"'.format(result_str) ) 
+    if os.name == 'posix':
+        os.system(
+            '[ -x $PREFIX/libexec/termux-api ]&&termux-notification -t "{}"'.format(result_str))
 #    elseif os.name == 'nt' : 这里打算做win10toast
 
-    #Telegram push by Yuzi0201
+    # Telegram push by Yuzi0201
     if os.getenv('TELETOKEN') and os.getenv('TELECHATID'):
-        token=os.getenv('TELETOKEN')
-        chat_id=os.getenv('TELECHATID')
-        #设置代理，如服务器在境外则不需要
+        token = os.getenv('TELETOKEN')
+        chat_id = os.getenv('TELECHATID')
+        # 设置代理，如服务器在境外则不需要
         #proxy = telegram.utils.request.Request(proxy_url='http://192.168.209.1:1081')
         #bot = telegram.Bot(token, request=proxy)
         bot = telegram.Bot(token)
         utc_dt = datetime.utcnow().replace(tzinfo=timezone.utc)
         bj_dt = utc_dt.astimezone(timezone(timedelta(hours=8)))
-        if "," in chat_id:#多用户支持
-            chat_id=chat_id.split(",")
+        if "," in chat_id:  # 多用户支持
+            chat_id = chat_id.split(",")
             for id in chat_id:
-                bot.send_message(id, text=str(bj_dt.month)+"月"+str(bj_dt.day)+"日的打卡结果：\n"+result_str)
+                bot.send_message(id, text=str(bj_dt.month) +
+                                 "月"+str(bj_dt.day)+"日的打卡结果：\n"+result_str)
         else:
-            bot.send_message(chat_id, text=str(bj_dt.month)+"月"+str(bj_dt.day)+"日的打卡结果：\n"+result_str)
+            bot.send_message(chat_id, text=str(bj_dt.month) +
+                             "月"+str(bj_dt.day)+"日的打卡结果：\n"+result_str)
 
 
 main()
